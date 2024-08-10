d13450d6b3802bfac141b702ce8f3c3f77393e12
Jacob Singer <jacobleesinger@gmail.com>
2024-08-09 20:55:22 -0700

    feat: add results

---
+++ src/lib/components/item.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let item: any;
+</script>
+
+<section class="card bg-base-100 w-96 shadow-xl">
+	<div class="card-body">
+		<figure>
+			<img src={item.imageUrl} alt={item.name} height="50" width="50" />
+		</figure>
+		<h2 class="card-title">{item.name}</h2>
+
+		<p class="card-text">{item.description}</p>
+
+		<section class="card-actions justify-end">
+			<button class="btn btn-primary">Add to Cart</button>
+		</section>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import algoliasearch from 'algoliasearch/lite';
+	import Item from '$lib/components/item.svelte';
 
 	const ALGOLIA_APPLICATION_ID = '6HX33Z8XEZ';
 	const ALGOLIA_SEARCH_KEY = '8c07b352caf4d761e8c83d0d1752b41f';
@@ -8,6 +9,7 @@
 
 	const index = searchClient.initIndex('itemData');
 
+	let results: any[] = [];
 	const search = async (e: Event) => {
 		console.log('search');
 		const { value } = e.target as HTMLInputElement;
@@ -15,7 +17,18 @@
 		const { hits } = await index.search(value);
 
 		console.log('hits', hits);
+
+		results = hits;
 	};
 </script>
 
-<input type="text" class="input input-bordered" oninput={search} />
+<main>
+	<input type="text" class="input input-bordered" oninput={search} />
+
+	<h2>Results</h2>
+	<section class="flex flex-wrap gap-4">
+		{#each results as result}
+			<Item item={result} />
+		{/each}
+	</section>
+</main>
