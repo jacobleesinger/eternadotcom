133c75d1c77c64aea6ee9225206b272aa028e5f1
Jacob Singer <jacobleesinger@gmail.com>
2024-08-09 20:26:52 -0700

    feat: add algolia search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import algoliasearch from 'algoliasearch/lite';
+
+	const ALGOLIA_APPLICATION_ID = '6HX33Z8XEZ';
+	const ALGOLIA_SEARCH_KEY = '8c07b352caf4d761e8c83d0d1752b41f';
+
+	const searchClient = algoliasearch(ALGOLIA_APPLICATION_ID, ALGOLIA_SEARCH_KEY);
+
+	const index = searchClient.initIndex('itemData');
+
+	const search = async (e: Event) => {
+		console.log('search');
+		const { value } = e.target as HTMLInputElement;
+		console.log('value', value);
+		const { hits } = await index.search(value);
+
+		console.log('hits', hits);
+	};
+</script>
+
+<input type="text" class="input input-bordered" oninput={search} />
